5b2eaf7346384f2a6767622fcbf1ae6ea81120f2
Stojance Demirovic <sturlei94@gmail.com>
2020-12-11 16:46:34 +0100

    added themes

--- src/style/theme/Themes.jsx
+++ src/style/theme/Themes.jsx
@@ -66,4 +66,36 @@ allThemes.Red = createGlobalStyle`
 
 `;
 
+allThemes.Green = createGlobalStyle`
+
+    /* colors */
+    :root {
+        --color-primary-default: #82e631;
+        --color-primary-light: #6edc7d;
+        --color-primary-dark: #5ba912;
+
+        --color-secondary-default: #6d6bfa;
+        --color-secondary-light: #8486fa;
+        --color-secondary-dark: #5329e9;
+        
+        /* shades */
+        --color-white-1: #ffffff;
+        --color-white-2: #f5f5f5;
+        --color-white-3: #ececec;
+     
+        --color-black-1: #000000;
+        --color-black-2: #141414;
+        --color-black-3: #242424;
+
+         --color-gray-1: #212121;
+        --color-gray-2: #575757;
+        --color-gray-3: #acacac;
+
+        /* positive colors */
+        --color-positive-1: #a0e72e;
+    }
+    
+
+`;
+
 export default allThemes;
