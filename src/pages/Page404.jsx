e54a8995bd259241f191670a31afc3430860bf67
Stojan <sturlei94@gmail.com>
2020-12-12 15:07:49 +0100

    made changes on the 404 page

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -4,9 +4,19 @@ import styled from "styled-components";
 const Page404 = () => {
     return (
         <PageWarper>
-            <h1>The page you are looking for</h1>
-            <p>does not exist</p>
-            <p>Go back to the <Link to="/">frontpage</Link></p>
+            <LeftSide>
+                <h1>The page you are looking for</h1>
+                <p>does not exist</p>
+                <p>Go back to the <Link to="/">frontpage</Link></p>
+            </LeftSide>
+            <RightSide>
+                <Title404>
+                    404
+                </Title404>
+                <SubTitle404>
+                    Page not found
+                </SubTitle404>
+            </RightSide>
         </PageWarper>
     )
 }
@@ -18,9 +28,40 @@ const PageWarper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
-
+    flex-direction: row;
+    /* background-image: url("/assets/404/page.svg"); */
+    background-position: center center;
+    background-size: 80% auto;
+    background-repeat: no-repeat;
 `;
+const LeftSide = styled.div`
+    display: flex;
+    height: 100vh;
+    flex: 1 1 50%;
+    flex-direction: column;
+    justify-content: center;
+
+
+    padding-left: 10rem;
+
+`
+const RightSide = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 50%;
+    justify-content: center;
+    align-items: center;
+
+`
+const Title404 = styled.h1`
+    font-size: 20rem;
+    line-height: 1;
+
+`
+const SubTitle404 = styled.p`
+font-size: 3rem;
+line-height: 1;
+`
 
 
 
