fa70d7be37759fb0fe62b2da158c637ccd57ea0d
Stojan <sturlei94@gmail.com>
2020-12-12 22:27:32 +0100

    Added sprites

---
+++ src/Sprites/SpriteOneCollection.jsx
@@ -0,0 +1,12 @@
+
+const style = {
+    display: "none"
+}
+const Sprite = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" style={style}><symbol id="notification" viewBox="0 0 32 32">
+        <title>notification</title>
+        <path d="m18.82 27a3 3 0 0 1 -5.64 0zm11.18-3h-3v-10c0-6.62-4.93-12-11-12s-11 5.38-11 12v10h-3a1 1 0 0 0 0 2h28a1 1 0 0 0 0-2z" /></symbol>
+    </svg>
+);
+
+export default Sprite;
