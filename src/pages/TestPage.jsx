9d2fae1e17d6b7f95d2f889ac00b2eda654e9671
Stojan <sturlei94@gmail.com>
2020-12-18 08:01:16 +0100

    Test

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -7,23 +7,29 @@ import styled from "styled-components";
 import NotificationList from "../components/Notifications/NotificationList";
 const TestPage = () => {
     const [active, setActive] = useState( -1 );
-    const notifications = [
+    const [notifications, setNotifications] = useState( [
         {
             name: 'Att ringa',
-            number: 2,
+
             list: [
-                {subject: 'Vasil Bygg AB', message: 'Kontakta kunden, vill ha hjälp med hemsidan.'},
-                {subject: 'Marshala', message: 'Jabadaba du'},
+                { subject: 'Vasil Bygg AB', message: 'Kontakta kunden, vill ha hjälp med hemsidan.' },
+                { subject: 'Marshala', message: 'Jabadaba du' },
+                { subject: 'Ny kund', message: 'ny kund som vill utöka...' },
             ]
         },
         {
             name: 'Att göra',
-            number: 8,
-             list: [
-                {subject: 'Content Writer', message: 'Hitta en anställd asap'}
+            list: [
+                { subject: 'Content Writer', message: 'Hitta en anställd asap' }
             ]
         },
-    ]
+    ] );
+
+    const removeItem = ( index ) => {
+        let data = [...notifications];
+        data[active].list.splice( index, 1 );
+        setNotifications( data );
+    }
 
     return (
         <PageContainer>
@@ -43,6 +49,7 @@ const TestPage = () => {
                 active={active}
                 close={setActive}
                 open={setActive}
+                removeItem={removeItem}
             />
         </PageContainer>
 
