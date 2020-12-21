acc22bc2bea1875eba8bd328d10dc757a3726b52
Stojan <sturlei94@gmail.com>
2020-12-22 00:53:34 +0100

    Added to index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,14 +6,46 @@ import styled from "styled-components";
 const Index = () => {
 
     return (
-        <div >
-            <SiteTitle>This is the index page</SiteTitle>
-        </div>
+        <PageWrapper>
+            <FirstSection>
+                <FirstTitle>
+                    Sturlei Web page
+                </FirstTitle>
+            </FirstSection>
+
+        </PageWrapper>
     )
 };
 
-const SiteTitle = styled.h1`
-    color: var(--color-primary-default);
+const FirstTitle = styled.h1`
+    position: relative;
+    font-size: 5rem;
+    color: var(--color-black-3);
+    &:after {
+        content: '';
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: .4rem;
+        background-color: currentColor;
+    }
+`;
+const FirstSection = styled.div`
+    padding: 10rem;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    background-color: var(--color-white-1);
+    width: 100%;
+    height: 80vh;
+    background-image: linear-gradient( 150deg, var(--color-white-1) 40%, transparent),url("/assets/other/background.svg");
+    background-position: 20% center;
+    background-repeat: no-repeat;
+    background-size: 140% auto;
+
+`;
+const PageWrapper = styled.div`
 `;
 
 
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 import styled from "styled-components";
 
 
@@ -6,32 +6,32 @@ import styled from "styled-components";
 // import ScoreBar from "../components/Score/ScoreBar";
 import NotificationList from "../components/Notifications/NotificationList";
 const TestPage = () => {
-    const [active, setActive] = useState( -1 );
-    const [notifications, setNotifications] = useState( [
-        {
-            name: 'Att ringa',
-            list: [
-                { subject: 'Vasil Bygg AB', message: 'Kontakta kunden, vill ha hjälp med hemsidan.' },
-                { subject: 'Marshala', message: 'Jabadaba du' },
-                { subject: 'Ny kunds', message: 'ny kun som vill utöka...' },
-                { subject: 'Ny kundsa', message: 'ny kund sm vill utöka...' },
-                { subject: 'Ny kundsf', message: 'ny kund som vill töka...' },
-                { subject: 'Ny kundd', message: 'ny kund som vill utöa...' },
-            ]
-        },
-        {
-            name: 'Att göra',
-            list: [
-                { subject: 'Content Writer', message: 'Hitta en anställd asap' }
-            ]
-        },
-    ] );
+    // const [active, setActive] = useState( -1 );
+    // const [notifications, setNotifications] = useState( [
+    //     {
+    //         name: 'Att ringa',
+    //         list: [
+    //             { subject: 'Vasil Bygg AB', message: 'Kontakta kunden, vill ha hjälp med hemsidan.' },
+    //             { subject: 'Marshala', message: 'Jabadaba du' },
+    //             { subject: 'Ny kunds', message: 'ny kun som vill utöka...' },
+    //             { subject: 'Ny kundsa', message: 'ny kund sm vill utöka...' },
+    //             { subject: 'Ny kundsf', message: 'ny kund som vill töka...' },
+    //             { subject: 'Ny kundd', message: 'ny kund som vill utöa...' },
+    //         ]
+    //     },
+    //     {
+    //         name: 'Att göra',
+    //         list: [
+    //             { subject: 'Content Writer', message: 'Hitta en anställd asap' }
+    //         ]
+    //     },
+    // ] );
 
-    const removeItem = ( index ) => {
-        let data = [...notifications];
-        data[active].list.splice( index, 1 );
-        setNotifications( data );
-    }
+    // const removeItem = ( index ) => {
+    //     let data = [...notifications];
+    //     data[active].list.splice( index, 1 );
+    //     setNotifications( data );
+    // }
 
     return (
         <PageContainer>
@@ -46,13 +46,13 @@ const TestPage = () => {
                 textOne="Your score"
                 description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, vitae!"
             /> */}
-            <NotificationList
+            {/* <NotificationList
                 items={notifications}
                 active={active}
                 close={setActive}
                 open={setActive}
                 removeItem={removeItem}
-            />
+            /> */}
         </PageContainer>
 
     )
