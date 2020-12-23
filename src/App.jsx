91bc54349efee480258bb5238d23f562a62e46f0
Stojance Demirovic <sturlei94@gmail.com>
2020-12-23 16:55:44 +0100

    Added: Added settings icon and redux connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,96 @@
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
+import styled from "styled-components";
+import {  motion} from "framer-motion";
 // styles
 import BaseStyle from "./style/BaseStyle";
 import GlobalVariables from "./style/GlobalVariables";
 import Themes from "./style/theme/Themes";
 // sprites
 import SpriteOneCollection from "./Sprites/SpriteOneCollection";
+// components
+import IconBars from "./components/Bars/IconBars";
+import SVG from "./components/SVG/SVG";
+// minipages
+import SettingsMiniPage from "./miniPages/Settings";
+// actions
+import { actions } from "./store/settings";
 
 function App( props ) {
-  const { children } = props;
+  const { children, triggerSettings, showSettings } = props;
   const CurrnetTheme = Themes[props.settings.config.themes.currentTheme];
 
   return (
-    <div className="App" >
+    <div className="App"  >
       <BaseStyle />
       <GlobalVariables />
       <CurrnetTheme />
       <SpriteOneCollection />
+      <PositioningAbsolute >
+
+        {showSettings ? (
+          <SettingsPanel>
+              <ClosePanel onClick={()=> triggerSettings(false)}>
+              <SVG icon="close" />
+              </ClosePanel>
+          <SettingsMiniPage />
+        </SettingsPanel>
+        ) : (
+            <IconBars shadow items={[{
+                    icon: 'settings',
+                    name: 'Settings',
+                    onClick: ()=> triggerSettings()
+                }]}/>
+        )}
+
+
+       
+      
+      </PositioningAbsolute>
       {children}
     </div>
   );
 }
+const PositioningAbsolute = styled.div`
+  position: absolute;
+  bottom: 3rem;
+  right: 3rem;
+`;
+const SettingsPanel = styled(motion.div)`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  background-color: var(--color-white-2);
+  width: 30rem;
+  height: 50rem;
+  padding: 3rem 2rem;
+  border-radius: 2rem;
+  box-shadow: var(--box-shadow-1);
+`;
+const ClosePanel = styled.div`
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+    & svg{
+    width: 2rem;
+    height: 2rem;
+    fill: var(--color-primary-light);
+    transition: all .2s ease;
+    cursor: pointer;
+    &:hover {
+
+    fill: var(--color-primary-default);
+    }
+    }
+`;
 
 // state.entities.settings.config
 const mapStateToProps = ( state ) => ( {
-  settings: state.entities.settings
+  settings: state.entities.settings,
+  showSettings: state.entities.settings.showSettings
 } );
 const mapDispatchToProps = ( dispatch ) => ( {
+  triggerSettings: ()=> dispatch( actions.triggerSettings() )
 } )
 
 export default connect( mapStateToProps, mapDispatchToProps )( withRouter( App ) );
